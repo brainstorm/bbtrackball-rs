8b3d465da70e73b19cef4734273eaf36cdd1bc56
Roman Valls Guimera <brainstorm@nopcode.org>
2020-08-04 22:06:21 +1000

    Correctly define the rest of I/O, next up: interrupts and hall sensors

--- software/stm32f04/src/main.rs
+++ software/stm32f04/src/main.rs
@@ -36,36 +36,43 @@ fn main() -> ! {
     // Get delay provider
     let mut delay = Delay::new(cp.SYST, &rcc);
 
-    // Configure the on-board LED (green) and spare buttons
+    // Configure the on-board I/O: LEDs, spare buttons and hall sensors
     // ..."split() takes the raw peripheral struct from the PAC
     // and converts it into a struct that provides separate access and ownership for each GPIO pin"
     // https://craigjb.com/2019/12/31/stm32l0-rust/
-    let gpiob = p.GPIOB.split(&mut rcc);
-    let gpioa = p.GPIOA.split(&mut rcc);
 
-    let (mut usr_led, _button3, _button4, _button5) = cortex_m::interrupt::free(|cs| {
+    // USR LED and Buttons
+    let gpiob = p.GPIOB.split(&mut rcc);
+    let (mut usr_led, mut _button3, mut _button4, mut _button5) = cortex_m::interrupt::free(|cs| {
         (
-            //bbled_red, bbled_grn, bbled_blu, bbled_wht)
             gpiob.pb1.into_push_pull_output(cs),
-            gpiob.pb5.into_push_pull_output(cs),
-            gpiob.pb4.into_push_pull_output(cs),
-            gpiob.pb3.into_push_pull_output(cs),
-            // gpioa.pa1.into_push_pull_output(cs),
-            // gpioa.pa2.into_push_pull_output(cs),
-            // gpioa.pa3.into_push_pull_output(cs),
-            // gpioa.pa4.into_push_pull_output(cs),
+            gpiob.pb5.into_pull_up_input(cs),
+            gpiob.pb4.into_pull_up_input(cs),
+            gpiob.pb3.into_pull_up_input(cs),
         )
     });
 
+    // LEDs and USB
+    let gpioa = p.GPIOA.split(&mut rcc);
+    let (mut bbled_red, mut bbled_grn, mut bbled_blu, mut bbled_wht, pin_dm, pin_dp) =
+        cortex_m::interrupt::free(|cs| {
+            (
+                gpioa.pa1.into_push_pull_output(cs),
+                gpioa.pa2.into_push_pull_output(cs),
+                gpioa.pa3.into_push_pull_output(cs),
+                gpioa.pa4.into_push_pull_output(cs),
+                gpioa.pa11,
+                gpioa.pa12,
+            )
+        });
+
     usr_led.set_low().ok(); // Turn off
 
-    let usb = Peripheral {
+    let usb_bus = UsbBus::new(Peripheral {
         usb: p.USB,
-        pin_dm: gpioa.pa11,
-        pin_dp: gpioa.pa12,
-    };
-
-    let usb_bus = UsbBus::new(usb);
+        pin_dm: pin_dm,
+        pin_dp: pin_dp,
+    });
 
     // Define USB device
     let mut _usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
@@ -77,6 +84,11 @@ fn main() -> ! {
 
     loop {
         usr_led.toggle().ok();
+        bbled_blu.toggle().ok();
+        bbled_grn.toggle().ok();
+        bbled_red.toggle().ok();
+        bbled_wht.toggle().ok();
+
         delay.delay_ms(1000u32);
     }
 }
