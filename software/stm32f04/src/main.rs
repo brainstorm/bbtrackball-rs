a67026ea1481f9689dfba314408a73ef9d4b4410
Roman Valls Guimera <brainstorm@nopcode.org>
2020-08-20 22:38:06 +1000

    Remove most of the bloat, HID can be implemented with leaner crate. Introduce RTT based debugging via probe-rs on the STLinkv2, focusing on external interrupts now.

--- software/stm32f04/src/main.rs
+++ software/stm32f04/src/main.rs
@@ -4,11 +4,9 @@
 #![no_main]
 
 use panic_halt as _;
-use rtt_target::{rtt_init_print};
-//use panic_semihosting as _;
+use rtt_target::{rtt_init_print, rprintln};
 use rtic::app;
 
-use embedded_hal::digital::v2::{InputPin, OutputPin};
 use stm32f0xx_hal::{
     prelude::*,
     pac,
@@ -17,14 +15,17 @@ use stm32f0xx_hal::{
 
 use usb_device::prelude::*;
 use usb_device::bus::UsbBusAllocator;
-use usb_device::class::UsbClass as _;
-use usbd_serial::{SerialPort, USB_CLASS_CDC};
-// 
-mod hid;
-mod trackball;
-mod button_scanner;
+// use usb_device::class::UsbClass as _;
 
-define_scanner!(Scanner, 2, 1);
+// use usbd_hid::hid_class::{HIDClass};
+// use usbd_hid::descriptor::MouseReport;
+// use usbd_hid::descriptor::generator_prelude::*;
+
+use usbd_serial::{SerialPort, USB_CLASS_CDC};
+
+//use cortex_m::asm::delay as cycle_delay;
+use cortex_m::interrupt::free as disable_interrupts;
+// use cortex_m::interrupt;
 
 #[app(device = stm32f0xx_hal::pac, peripherals = true)]
 const APP: () = {
@@ -32,15 +33,13 @@ const APP: () = {
         usb_bus: &'static UsbBusAllocator<usb::UsbBusType>,
         usb_serial: usbd_serial::SerialPort<'static, usb::UsbBusType>,
         usb_device: UsbDevice<'static, usb::UsbBusType>,
-        usb_trackball: hid::HidClass<'static, usb::UsbBusType, trackball::Trackball>,
-        //scanner: Scanner<Pxx<Input<PullDown>>, Pxx<Output<PushPull>>>,
     }
 
     #[init]
     fn init(ctx: init::Context) -> init::LateResources {
         static mut USB_BUS: Option<UsbBusAllocator<usb::UsbBusType>> = None;
 
-        // Panic RTT handler
+        // RTT handler
         rtt_init_print!();
 
         // Alias peripherals
@@ -52,6 +51,8 @@ const APP: () = {
         */
         dp.RCC.apb2enr.modify(|_, w| w.syscfgen().set_bit());
         dp.SYSCFG.cfgr1.modify(|_, w| w.pa11_pa12_rmp().remapped());
+
+        rprintln!("Initializing peripherals");
 
         let mut rcc = dp
             .RCC
@@ -65,7 +66,7 @@ const APP: () = {
 
         // Set up GPIO registers for USR LED and Buttons
         let gpiob = dp.GPIOB.split(&mut rcc);
-        let (mut _usr_led, mut _button3, mut _button4, mut _button5) = cortex_m::interrupt::free(|cs| {
+        let (mut _usr_led, mut _button3, mut _button4, mut _button5) = disable_interrupts (|cs| {
             (
                 gpiob.pb1.into_push_pull_output(cs),
                 gpiob.pb5.into_pull_up_input(cs),
@@ -77,7 +78,7 @@ const APP: () = {
         // LEDs and USB
         let gpioa = dp.GPIOA.split(&mut rcc);
         let (mut _bbled_red, mut _bbled_grn, mut _bbled_blu, mut _bbled_wht, usb_dm, usb_dp) =
-            cortex_m::interrupt::free(|cs| {
+            disable_interrupts (|cs| {
                 (
                     gpioa.pa1.into_push_pull_output(cs),
                     gpioa.pa2.into_push_pull_output(cs),
@@ -94,10 +95,11 @@ const APP: () = {
             pin_dp: usb_dp,
         };
 
-        let (usb_serial, usb_trackball, usb_device) = {
+        rprintln!("Defining USB parameters");
+
+        let (usb_serial, usb_device) = {
             *USB_BUS = Some(usb::UsbBus::new(usb));
             let serial = SerialPort::new(USB_BUS.as_ref().unwrap());
-            let trackball = hid::HidClass::new(trackball::Trackball::new(), USB_BUS.as_ref().unwrap());
             let usb_dev = UsbDeviceBuilder::new(
                         USB_BUS.as_ref().unwrap(),
                         UsbVidPid(0x16c0, 0x27dd)
@@ -108,39 +110,39 @@ const APP: () = {
                 .device_class(USB_CLASS_CDC)
                 .build();
 
-            (serial, trackball, usb_dev)
+            (serial, usb_dev)
         };
 
         init::LateResources {
             usb_bus: USB_BUS.as_ref().unwrap(),
             usb_serial,
             usb_device,
-            usb_trackball,
-            //scanner
         }
     }
-    #[idle(resources = [usb_serial, usb_trackball])]
-    fn idle(ctx: idle::Context) -> ! {
-        let mut r = ctx.resources;
-//        let mut last_raw: i32 = 0;
-        loop {
-            let button_buffer = [0; 3];
-            //r.scanner.scan_to_bytes(&mut button_buffer, 0);
-
-            let report = trackball::TrackballReport::new(10, 10, button_buffer);
-            r.usb_trackball.lock(|tball| {
-                tball.device_mut().set_report(report.clone());
-            });
-            while let Ok(0) = r.usb_trackball.lock(|tball| {
-                tball.write(&report.as_bytes())
-            })
-            {}
-        }
+    #[idle(resources = [usb_serial])]
+    fn idle(_: idle::Context) -> ! {
+        loop {}
     }
 
-    #[task(binds = USB, resources = [usb_device, usb_trackball, usb_serial])]
-    fn usbrx(cx: usbrx::Context) {
-        usb_poll(cx.resources.usb_device, cx.resources.usb_serial, cx.resources.usb_trackball);
+    #[task(binds = USB, resources = [usb_device, usb_serial])]
+    fn usbrx(ctx: usbrx::Context) {
+        usb_poll(ctx.resources.usb_device, ctx.resources.usb_serial);
+    }
+
+    #[task(binds = EXTI0_1)]
+    fn zerotoone(_: zerotoone::Context) {
+        rprintln!("Interrupts happening on EXTI0_1");
+    }
+
+    #[task(binds = EXTI2_3)]
+    fn twotothree(_: twotothree::Context) {
+        rprintln!("Interrupts happening on EXTI2_3");
+    }
+
+
+    #[task(binds = EXTI4_15)]
+    fn gpioa(_: gpioa::Context) {
+        rprintln!("Interrupts happening on EXTI4_15... pins from 4 to 15? PA OR PB? GPIOA or GPIOB?");
     }
     
     extern "C" {
@@ -148,19 +150,12 @@ const APP: () = {
     }
 };
 
+
 fn usb_poll<B: usb_device::bus::UsbBus>(
     usb_dev: &mut UsbDevice<'static, B>,
-    serial: &mut SerialPort<'static, B>,
-    trackball: &mut hid::HidClass<'static, B, trackball::Trackball>,
+    serial: &mut SerialPort<'static, B>
 ) {
-    if !usb_dev.poll(&mut [serial, trackball]) {
+    if !usb_dev.poll(&mut [serial]) {
         return;
-    }
-    trackball.poll();
-    let mut buf = [0;10];
-    match serial.read(&mut buf) {
-        Ok(_) => {},
-        Err(UsbError::WouldBlock) => {},
-        e => panic!("USB read error: {:?}", e)
     }
 }
